4d18a3dcdbd3b8686f70a3d181e1c40dea6f3f4d
steveraaen <sraaen@gmail.com>
2017-05-26 11:25:06 -0400

    further things worth saving

---
+++ bubble_graph.js
@@ -0,0 +1,24 @@
+
+
+var bubbleChart = function () {
+    var width = 600,
+    height = 400;
+
+    function chart(selection){
+        // you gonna get here
+    }
+    chart.width = function(value) {
+        if (!arguments.length) { return width; }
+        width = value;
+
+        return chart;
+    }
+    chart.height = function(value) {
+        if (!arguments.length) { return height; }
+        height = value;
+
+        return chart;
+    }
+
+    return chart;
+}
